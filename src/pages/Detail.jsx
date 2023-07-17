b0f6a2a05e2eb42d1bad833c1742319441f21ad6
Seryeong Lee <srlimvp@gmail.com>
2023-07-18 00:10:58 +0900

    Feat: kakao api 지도 불러오기 index.html body에 kakao api 입력 추가 Map.jsx에서 Map 불러오는 코드 작성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Map from '../components/Map/Map';
 
 function Detail() {
-  return <div>Detail</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 }
 
 export default Detail;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
+import Detail from '../pages/Detail';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
