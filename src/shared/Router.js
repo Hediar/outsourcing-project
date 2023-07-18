9ddcf68e0d2f7ac2fea996dd6686fc72a3f0730f
Lee Jinsol <jins2ol@naver.com>
2023-07-18 10:10:07 +0900

    Feat-move to Blog URL function
    a태그를 이용하여 블로그 박스를 클릭하면 해당 블로그로 이동하도록 기능 구현

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
 import Detail from '../pages/Detail';
+import GlobalStyle from '../style/GlobalStyle';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
