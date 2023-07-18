05112cd8492be95a014b70c3dc22b186f4223468
Seryeong Lee <srlimvp@gmail.com>
2023-07-18 10:24:11 +0900

    Merge: 블로그 검색 ShowBlogList, Blog 추

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,14 +5,18 @@ function Map() {
   const [map, setMap] = useState(null);
 
   useEffect(() => {
-    // console.log(window.kakao);
     const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3
     };
     const map = new kakao.maps.Map(container, options);
-    console.log(map);
+
+    const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
+    marker.setMap(map);
     setMap(map);
   }, []);
 
