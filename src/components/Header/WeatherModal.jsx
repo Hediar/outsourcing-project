ffd89ea02de39be09fda096b462327767111377a
Lee Jinsol <jins2ol@naver.com>
2023-07-21 19:48:32 +0900

    Refactor-wether data
    세령님께서 데이터를 가지고와서 가공하는 부분을 수정해주심 :)

--- src/components/Header/WeatherModal.jsx
+++ src/components/Header/WeatherModal.jsx
@@ -11,15 +11,32 @@ const WeatherModal = () => {
   const boxRef = useRef(null);
   const dispatch = useDispatch();
 
-  const { data, isError, isLoading } = useQuery('5DaysOfWeather', get5DaysOfWeather, {
-    onSuccess: () => {
-      // console.log('성공', data);
-    }
-  });
+  const { data, isError, isLoading } = useQuery('5DaysOfWeather', get5DaysOfWeather);
 
   const CloseModal = async () => {
     await gsap.to(boxRef.current, 0.3, { transform: 'translateY(-500px)', delay: 0, ease: 'ease' });
     dispatch(setWeatherModalOn(false));
+  };
+
+  const everyDays = data?.list.map((item) => item.dt_txt.substr(0, 10));
+  const days = [...new Set(everyDays)].splice(1, 6);
+
+  const weatherData = days?.map((day) => {
+    const dataForDay = data?.list?.filter((item) => item.dt_txt.includes(day));
+    return {
+      Max: dataForDay.map((item) => item.main.temp_max),
+      Min: dataForDay.map((item) => item.main.temp_min),
+      Icon: dataForDay[0]?.weather[0]?.icon ?? 'default-icon'
+    };
+  });
+
+  const tempMax = (a) => {
+    const numOnlyArr = a.filter((item) => typeof item == 'number');
+    return Math.round((Math.max(...numOnlyArr) - 273.15) * 10) / 10;
+  };
+  const tempMin = (b) => {
+    const numOnlyArr = b.filter((item) => typeof item == 'number');
+    return Math.round((Math.min(...numOnlyArr) - 273.15) * 10) / 10;
   };
 
   useEffect(() => {
@@ -32,95 +49,64 @@ const WeatherModal = () => {
   if (isError) {
     return <S.WeatherModalContainer>Error..</S.WeatherModalContainer>;
   }
-  const everyDays = data.list.map((item) => item.dt_txt.substr(0, 10));
-  const days = [...new Set(everyDays)];
-  // console.log('days', days);
-
-  const firstDayData = [];
-  const secondDayData = [];
-  const thirdDayData = [];
-  const fourthDayData = [];
-  const fifthDayData = [];
-
-  const pushData = data.list.forEach((item) => {
-    if (item?.dt_txt.includes(days[1])) {
-      firstDayData.push(item.main.temp_max);
-      firstDayData.push(item.main.temp_min);
-      firstDayData.push(item.weather[0].icon);
-    }
-    if (item.dt_txt.includes(days[2])) {
-      secondDayData.push(item.main.temp_max);
-      secondDayData.push(item.main.temp_min);
-      secondDayData.push(item.weather[0].icon);
-    }
-    if (item.dt_txt.includes(days[3])) {
-      thirdDayData.push(item.main.temp_max);
-      thirdDayData.push(item.main.temp_max);
-      thirdDayData.push(item.weather[0].icon);
-    }
-    if (item.dt_txt.includes(days[4])) {
-      fourthDayData.push(item.main.temp_max);
-      fourthDayData.push(item.main.temp_min);
-      fourthDayData.push(item.weather[0].icon);
-    }
-    if (item.dt_txt.includes(days[5])) {
-      fifthDayData.push(item.main.temp_max);
-      fifthDayData.push(item.main.temp_min);
-      fifthDayData.push(item.weather[0].icon);
-    }
-  });
-
-  console.log(fourthDayData);
-
-  const tempMax = (a) => {
-    const numOnlyArr = a.filter((item) => typeof item == 'number');
-    return Math.round((Math.max(...numOnlyArr) - 273.15) * 10) / 10;
-  };
-  const tempMin = (b) => {
-    const numOnlyArr = b.filter((item) => typeof item == 'number');
-    return Math.round((Math.min(...numOnlyArr) - 273.15) * 10) / 10;
-  };
 
   return (
-    <S.Container>
+    <>
       <S.WeatherModalContainer ref={boxRef}>
-        <div>
-          <div>
-            {days[1]}
-            {tempMax(firstDayData)}°C
-            {tempMin(firstDayData)}°C
-            <img src={`https://openweathermap.org/img/wn/${firstDayData[2]}@2x.png`} alt="Image" width="60px" />
-          </div>
-          <div>
-            {days[2]}
-            {tempMax(secondDayData)}°C
-            {tempMin(secondDayData)}°C
-            <img src={`https://openweathermap.org/img/wn/${secondDayData[2]}@2x.png`} alt="Image" width="60px" />
-          </div>
-          <div>
-            {days[3]}
-            {tempMax(thirdDayData)}°C
-            {tempMin(thirdDayData)}°C
-            <img src={`https://openweathermap.org/img/wn/${thirdDayData[2]}@2x.png`} alt="Image" width="60px" />
-          </div>
-          <div>
-            {days[4]}
-            {tempMax(fourthDayData)}°C
-            {tempMin(fourthDayData)}°C
-            <img src={`https://openweathermap.org/img/wn/${fourthDayData[2]}@2x.png`} alt="Image" width="60px" />
-          </div>
-          <div>
-            {days[5]}
-            {tempMax(fifthDayData)}°C
-            {tempMin(fifthDayData)}°C
-            <img src={`https://openweathermap.org/img/wn/${fifthDayData[2]}@2x.png`} alt="Image" width="60px" />
-          </div>
-        </div>
+        {weatherData?.map((item, i) => {
+          return (
+            <div>
+              <span>{days[i]}</span>
+              <span>{tempMax(item.Max)}</span>
+              <span>{tempMin(item.Min)}</span>
+              <img src={`https://openweathermap.org/img/wn/${item.Icon}@2x.png`} alt="Image" width="60px" />
+            </div>
+          );
+        })}
       </S.WeatherModalContainer>
-      <S.DetailCloseBtn onClick={CloseModal}>X</S.DetailCloseBtn>
-      <S.Bg onClick={CloseModal}></S.Bg>
-    </S.Container>
+    </>
   );
+
+  // return (
+  //   <S.Container>
+  //     <S.WeatherModalContainer ref={boxRef}>
+  //       <div>
+  //         <div>
+  //           {days[1]}
+  //           {tempMax(firstDayData)}°C
+  //           {tempMin(firstDayData)}°C
+  //           <img src={`https://openweathermap.org/img/wn/${firstDayData[2]}@2x.png`} alt="Image" width="60px" />
+  //         </div>
+  //         <div>
+  //           {days[2]}
+  //           {tempMax(secondDayData)}°C
+  //           {tempMin(secondDayData)}°C
+  //           <img src={`https://openweathermap.org/img/wn/${secondDayData[2]}@2x.png`} alt="Image" width="60px" />
+  //         </div>
+  //         <div>
+  //           {days[3]}
+  //           {tempMax(thirdDayData)}°C
+  //           {tempMin(thirdDayData)}°C
+  //           <img src={`https://openweathermap.org/img/wn/${thirdDayData[2]}@2x.png`} alt="Image" width="60px" />
+  //         </div>
+  //         <div>
+  //           {days[4]}
+  //           {tempMax(fourthDayData)}°C
+  //           {tempMin(fourthDayData)}°C
+  //           <img src={`https://openweathermap.org/img/wn/${fourthDayData[2]}@2x.png`} alt="Image" width="60px" />
+  //         </div>
+  //         <div>
+  //           {days[5]}
+  //           {tempMax(fifthDayData)}°C
+  //           {tempMin(fifthDayData)}°C
+  //           <img src={`https://openweathermap.org/img/wn/${fifthDayData[2]}@2x.png`} alt="Image" width="60px" />
+  //         </div>
+  //       </div>
+  //     </S.WeatherModalContainer>
+  //     <S.DetailCloseBtn onClick={CloseModal}>X</S.DetailCloseBtn>
+  //     <S.Bg onClick={CloseModal}></S.Bg>
+  //   </S.Container>
+  // );
 };
 
 const S = {
