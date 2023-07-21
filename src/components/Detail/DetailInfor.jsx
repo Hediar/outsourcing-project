27d54b45a3e3677db8d2baf7b2bc03618a6d828b
KimYoonSu97 <zkzk625@naver.com>
2023-07-21 12:42:31 +0900

    Refactor : 스타일 컴포넌트 정리
    1. 각 폴더별로 폴더명styled.jsx 파일 생성
    2. 많이 쓰이고 반복되는 display / align~~ / justify~~ 세 속성 묶어서 활용

--- src/components/Detail/DetailInfor.jsx
+++ src/components/Detail/DetailInfor.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { styled } from 'styled-components';
+import { S } from './DetailStyled';
 import { FaMapLocationDot, FaEarthAsia, FaClock, FaPhone, FaCircleInfo, FaSquareParking } from 'react-icons/fa6';
 const DetailInfor = () => {
   const { detailModalData } = useSelector((state) => state.detailModal);
 
   return (
     <S.InfoBox>
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaMapLocationDot color="orange" size="20px" />
         <S.Title>주소</S.Title>
       </S.InfoTitle>
       <S.Info>{detailModalData.address}</S.Info>
 
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaClock color="orange" size="20px"></FaClock>
         <S.Title>운영시간</S.Title>
       </S.InfoTitle>
       <S.Info>{detailModalData.detail?.businessHours}</S.Info>
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaPhone color="orange" size="20px" />
         <S.Title>전화번호</S.Title>
       </S.InfoTitle>
       <S.Info>{detailModalData.detail?.tel}</S.Info>
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaEarthAsia color="orange" size="20px" />
         <S.Title>홈페이지</S.Title>
       </S.InfoTitle>
@@ -34,12 +34,12 @@ const DetailInfor = () => {
           <a href={detailModalData.detail?.Link}>{detailModalData.detail?.Link}</a>
         )}
       </S.Info>
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaSquareParking color="orange" size="20px" />
         <S.Title>주차여부</S.Title>
       </S.InfoTitle>
       <S.Info>{detailModalData.detail?.parking ? '가능' : '불가능'}</S.Info>
-      <S.InfoTitle>
+      <S.InfoTitle as="h3">
         <FaCircleInfo color="orange" size="20px" />
         <S.Title>소개</S.Title>
       </S.InfoTitle>
@@ -47,43 +47,6 @@ const DetailInfor = () => {
       <S.InfoEmptyBox></S.InfoEmptyBox>
     </S.InfoBox>
   );
-};
-
-const S = {
-  InfoBox: styled.div`
-    margin: 0 20px;
-    height: calc(100vh - 385px);
-    overflow-y: scroll;
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  `,
-
-  Info: styled.p`
-    font-size: 16px;
-    text-align: center;
-    margin-bottom: 15px;
-    line-height: 1.8;
-  `,
-  InfoTitle: styled.h3`
-    font-size: 18px;
-    font-weight: bold;
-    text-align: left;
-    margin: 10px;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    justify-content: center;
-  `,
-  Title: styled.div`
-    color: orange;
-
-    padding-top: 4px;
-  `,
-  InfoEmptyBox: styled.div`
-    width: 100px;
-    height: 100px;
-  `
 };
 
 export default DetailInfor;
---
+++ src/components/Header/HeaderStyled.jsx
@@ -0,0 +1,52 @@
+import { styled } from 'styled-components';
+
+const S = {
+  Header: styled.header`
+    width: 100vw;
+    background-color: #ffa500;
+    position: fixed;
+    height: 70px;
+    z-index: 99;
+    box-sizing: border-box;
+  `,
+  Logo: styled.div`
+    position: fixed;
+    left: 50%;
+    top: 35px;
+    transform: translate(-50%, -50%);
+    width: 150px;
+    height: 30px;
+    background-image: url('https://velog.velcdn.com/images/lhb971219/post/c42592a0-754a-43d9-85fc-3a4287975ab6/image.png');
+    background-size: cover;
+    background-position: center;
+  `,
+  SelectButtons: styled.div`
+    display: flex;
+    position: absolute;
+    right: 3%;
+    top: 20%;
+  `,
+  ButtonStyle: styled.button`
+    cursor: pointer;
+    border: none;
+    width: 80px;
+    height: 30px;
+    margin: 6px;
+    border-radius: 10px;
+    border: 1px solid white;
+    background-color: #ffffff;
+    color: #ffa500;
+    &:hover {
+      box-shadow: 0 40px 12px #ffa500 inset, 0 -40px 12px #ffa500 inset;
+      color: rgba(255, 255, 255, 1);
+      transition: all 0.5s;
+      transform: scale(1.1);
+      font-weight: bold;
+    }
+    &:active {
+      transform: scale(1);
+    }
+  `
+};
+
+export { S };
